6f5ada4f9d7b4af0221492215b3fd5a885de6ed0
徐溶延 <xry@xurongyandeMacBook-Pro.local>
2019-07-08 09:48:28 +0800

    Initial commit

---
+++ exp_1_1_bisec.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def f(x):
+    return x ** 3 + x - 1
+
+
+def bisec(f, a, b, tol):
+    if np.sign(f(a)) * np.sign(f(b)) >= 0:
+        print("f(a)*f(b)<0 isn't satisfied")
+        return
+    while (b - a) / 2 > tol:
+        c = (b + a) / 2
+        if f(c) == 0:
+            break
+        elif np.sign(f(a)) * np.sign(f(c)) < 0:
+            b = c
+        else:
+            a = c
+    return (a + b) / 2
+
+
+if __name__ == '__main__':
+    xc = bisec(f, 0, 1, 0.00005)
+    print(xc)
+    print(f(xc))
