4dd8d4e57181f643ca3ae9ff51a2f9c29f415498
xry <1536980672@qq.com>
2019-07-22 13:27:23 +0800

    添加了2.5小节雅可比，高斯-塞德尔和SOR方法的实现

--- exp_2_5.py
+++ exp_2_5.py
@@ -2,22 +2,21 @@ import numpy as np
 
 tol = 0.5e-8
 
+
 def jacobi(A, b):
     D = np.zeros(A.shape)
-    L = np.zeros(A.shape)
-    U = np.zeros(A.shape)
+    R = np.zeros(A.shape)
     u = np.zeros(A.shape[0])
     for i in range(A.shape[0]):
         for j in range(A.shape[1]):
-            if i < j:
-                U[i, j] = A[i, j]
-            elif i > j:
-                L[i, j] = A[i, j]
+            if i != j:
+                R[i, j] = A[i, j]
             else:
                 D[i, j] = A[i, j]
     while np.abs(np.max(np.dot(A, u) - b)) > tol:
-        u = np.linalg.inv(D).dot(b - np.dot(L + U, u))
+        u = np.linalg.inv(D).dot(b - np.dot(R, u))
     return u
+
 
 def gauss_seidel(A, b):
     D = np.zeros(A.shape)
@@ -30,11 +29,11 @@ def gauss_seidel(A, b):
             else:
                 D[i, j] = A[i, j]
     D_inv = np.linalg.inv(D)
-
     while np.abs(np.max(np.dot(A, u) - b)) > tol:
         for i in range(A.shape[0]):
             u[i] = D_inv[i, i] * (b[i] - np.dot(R[i], u.transpose()))
     return u
+
 
 def SOR(A, b, w):
     D = np.zeros(A.shape)
@@ -47,11 +46,11 @@ def SOR(A, b, w):
             else:
                 D[i, j] = A[i, j]
     D_inv = np.linalg.inv(D)
-
     while np.abs(np.max(np.dot(A, u) - b)) > tol:
         for i in range(A.shape[0]):
             u[i] = (1 - w) * u[i] + w * (D_inv[i, i] * (b[i] - np.dot(R[i], u.transpose())))
     return u
+
 
 if __name__ == '__main__':
     A = np.array([[3, 1],
